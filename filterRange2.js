2c2b0724309289fb6b38af49116636bf729a1688
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-09-01 17:38:07 +0545

    ex 3

--- filterRange2.js
+++ filterRange2.js
@@ -1,5 +1,5 @@
-function myFilter(arr, a, b){
-
+function myFilter(arr, a, b) {
+    return arr.filter((element) => element >= a && element <= b);
 }
 
-console.log(myFilter([5, 3, 8, 1], 1, 4))
+console.log(myFilter([5, 3, 8, 1], 1, 4)); // Output: [3, 1]
