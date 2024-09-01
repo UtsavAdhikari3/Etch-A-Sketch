3bc3fd63e9bbc8e97491e631dd3b0d10930da119
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-09-01 17:25:03 +0545

    filterRange exercise

---
+++ filterRange.js
@@ -0,0 +1,6 @@
+function filterRange(arr, a, b){
+    filteredArray = arr.filter(element => element >= a && element <=b)
+    return filteredArray;
+}
+
+console.log(filterRange([5, 3, 8, 1], 1, 4));
