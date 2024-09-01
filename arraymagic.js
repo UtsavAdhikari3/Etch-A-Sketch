57c12ddc0a6ecd9f389419a1c42a21d706548df1
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-09-01 08:02:06 +0545

    objects array magic practice exercise

---
+++ arraymagic.js
@@ -0,0 +1,23 @@
+
+let tripleArray = [];
+let sum = 0;
+let noSum = 0;
+
+function sumOfTripledEvens(arr){
+    arr.forEach(element => {
+        let triple = element * 3;
+        tripleArray.push(triple);
+    });
+    tripleArray.forEach(item => {
+        if (item % 2 === 0){
+            sum = sum + item
+        }
+        else{
+            noSum = noSum + item;
+        }
+    })
+    return sum;
+}
+
+
+console.log(sumOfTripledEvens([1,2,3,4,5]));
